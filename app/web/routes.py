4bae31b04a780ca14c08d4f940fd6995afafbf03
S1riyS <kirill.ankudinov.94@mail.ru>
2021-11-24 15:16:35 +0500

    Added draft version of task page

--- app/web/routes.py
+++ app/web/routes.py
@@ -7,6 +7,12 @@ from . import app
 def home_page():
     return render_template('home.html')
 
+
 @app.route('/lessons')
 def lessons_page():
     return render_template('lessons.html')
+
+
+@app.route('/lessons/<int:grade>-grade/<string:topic>/<int:task_number>')
+def task_page(grade, topic, task_number):
+    return render_template('task.html', grade=grade, topic=topic, task_number=task_number)
