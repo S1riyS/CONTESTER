475041dfc338d9a007b12e6252a955650808a94f
S1riyS <kirill.ankudinov.94@mail.ru>
2021-11-30 16:16:36 +0500

    The sidebar is fixed when scrolling

---
+++ app/web/static/js/task.js
@@ -0,0 +1,33 @@
+let currentSidebarY = -1;
+
+$(function () {
+    $(window).scroll(function () {
+        let sidebar = $('#sidebar');
+        let task = $('#task');
+        console.log(currentSidebarY)
+
+        if (currentSidebarY <= 15) currentSidebarY = findPosY(sidebar);
+
+        if (pageYOffset > currentSidebarY - 15) {
+            sidebar.css({
+                position: 'fixed',
+                top: 15,
+                height: 'auto',
+            })
+            task.addClass("offset-xl-2");
+        } else {
+            sidebar.css({
+                position: 'relative',
+                top: 0,
+                height: '100%',
+            })
+            task.removeClass("offset-xl-2");
+        }
+
+    });
+});
+
+function findPosY(obj) {
+    let offset = obj.offset()
+    return offset.top - $(window).scrollTop()
+}
