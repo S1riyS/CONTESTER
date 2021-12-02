271adff074efb85e7f0df73ced41c3ec4d5de79a
S1riyS <kirill.ankudinov.94@mail.ru>
2021-12-02 11:40:47 +0500

    Changed paths

--- app/web/routes.py
+++ app/web/routes.py
@@ -9,18 +9,18 @@ def home_page():
     return render_template('home.html')
 
 
-@app.route('/lessons')
+@app.route('/all-tasks')
 def lessons_page():
     return render_template('lessons.html')
 
-@app.route('/lessons/<int:grade>-grade')
+@app.route('/<int:grade>-grade')
 def grade_page(grade):
     return render_template('grade.html', grade=grade)
 
-@app.route('/lessons/<int:grade>-grade/<string:topic>')
+@app.route('/<int:grade>-grade/<string:topic>')
 def topic_page(grade, topic):
     return render_template('topic.html', grade=grade, topic=topic)
 
-@app.route('/lessons/<int:grade>-grade/<string:topic>/<int:task_number>')
+@app.route('/<int:grade>-grade/<string:topic>/<int:task_number>')
 def task_page(grade, topic, task_number):
     return render_template('task.html', grade=grade, topic=topic, task_number=task_number)
