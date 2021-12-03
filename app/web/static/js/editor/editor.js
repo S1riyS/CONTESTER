0b70d1084ae3fc80e5b5d9ef8af42005e06e8b4a
S1riyS <kirill.ankudinov.94@mail.ru>
2021-12-03 12:07:31 +0500

    Restructured code, now code saves on page unloading

--- app/web/static/js/editor/editor.js
+++ app/web/static/js/editor/editor.js
@@ -1,61 +1,86 @@
-// Loading CodeMirror editor
-window.onload = function () {
+let codeMirrorConfig = {
+    mode: "python", // Language mode
+    theme: "material-palenight", // theme
+    lineNumbers: true, // set number
+    smartIndent: true, // smart indent
+    indentUnit: 4, // Smart indent in 4 spaces
+    indentWithTabs: true, // Smart indent with tabs
+    lineWrapping: true, //
+    // Add line number display, folder and syntax detector to the slot
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"],
+    foldGutter: true, // Enable code folding in slots
+    autofocus: true, // Autofocus
+    matchBrackets: true, // Match end symbols, such as "],}"
+    autoCloseBrackets: true, // Auto close symbol
+    styleActiveLine: true, // Display the style of the selected row
+    scrollbarStyle: 'simple' // Scrollbar style
+}
+
+function loadCodeMirror(config) {
+    // Configuring CodeMirror
     let editor = document.getElementById("editor__body");
-    let myCodeMirror = CodeMirror(editor, {
-        mode: "python", // Language mode
-        theme: "material-palenight", // theme
-        lineNumbers: true, // set number
-        smartIndent: true, // smart indent
-        indentUnit: 4, // Smart indent in 4 spaces
-        indentWithTabs: true, // Smart indent with tabs
-        lineWrapping: true, //
-        // Add line number display, folder and syntax detector to the slot
-        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"],
-        foldGutter: true, // Enable code folding in slots
-        autofocus: true, // Autofocus
-        matchBrackets: true, // Match end symbols, such as "],}"
-        autoCloseBrackets: true, // Auto close symbol
-        styleActiveLine: true, // Display the style of the selected row
-        scrollbarStyle: 'simple' // Scrollbar style
-    });
+    let myCodeMirror = CodeMirror(editor, config);
 
-    $('#submit-code__btn').click(function () {
-        let language = $('#dropdownMenuLanguage').find('.active')[0];
-        let path = window.location.pathname.split('/')
-        let request = {
-            code: myCodeMirror.getValue(),
-            lang: language.innerHTML,
-            task: {
-                grade: path[1],
-                topic: path[2],
-                task_number: path[3]
-            }
-        }
+    // Loading code from local storage
+    let codeMirrorText = localStorage.getItem('codeMirrorText');
+    let initEditorValue;
+    if (codeMirrorText) {
+        initEditorValue = codeMirrorText
+    } else {
+        initEditorValue = ''
+    }
+    // Setting saved code
+    myCodeMirror.setOption("value", initEditorValue);
 
-        $('#code-response').innerHTML = '';
-
-        // Sending AJAX response to server
-        $.ajax({
-            url: '/api/send_code',
-            type: 'POST',
-            contentType: 'application/json;charset=UTF-8',
-            data: JSON.stringify(request),
-            // Everything OK
-            success: function (response) {
-                $('#code-response').replaceWith(response)
-            },
-            error: function () {
-                console.log('Error')
-            }
-        })
+    // Saving code everytime code changes
+    myCodeMirror.on('change', function () {
+        localStorage.setItem('codeMirrorText', myCodeMirror.getValue())
     })
-};
+
+    return myCodeMirror
+}
+
+myCodeMirror = loadCodeMirror(codeMirrorConfig)
+
+// On click on submit button
+$('#submit-code__btn').click(function () {
+    let language = $('#dropdownMenuLanguage').find('.active')[0];
+    let path = window.location.pathname.split('/')
+    let request = {
+        code: myCodeMirror.getValue(),
+        lang: language.innerHTML,
+        task: {
+            grade: path[1],
+            topic: path[2],
+            task_number: path[3]
+        }
+    }
+
+    // Sending AJAX response to server
+    $.ajax({
+        url: '/api/send_code',
+        type: 'POST',
+        async: true,
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify(request),
+        // Before send
+        beforeSend: function () {
+            $('#code-response').html('');
+        },
+        // Everything OK
+        success: function (response) {
+            $('#code-response').replaceWith(response)
+        },
+        error: function () {
+            console.log('Error')
+        }
+    })
+})
 
 // On dropdown child click do...
 $(".dropdown-menu a").click(function () {
     // Remove any existing 'active' classes...
     $(this).closest('.dropdown-menu').find('a').removeClass('active');
-
     // Add 'active' class to clicked element...
     $(this).addClass('active');
 });
