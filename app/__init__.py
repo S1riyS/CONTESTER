9aaa01e24f928e5dee68cbaabff025da65de81f6
S1riyS <kirill.ankudinov.94@mail.ru>
2021-12-08 21:24:25 +0500

    Added libraries that necessary to backend part

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_moment import Moment
+from flask_migrate import Migrate
 
+from config import Config
+
+# Initialization of app
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+# login_manager = LoginManager(app)
+# moment = Moment(app)
+
 
 # Jinja2 global variables
 variables = {
-    'WEBSITE_URL': 'сontester17.herokuapp.com',
+    'WEBSITE_URL': 'contester.school17.perm.ru',
     'LANGUAGE': 'ru',
     'DEVELOPER': 'S1riyS'
 }
