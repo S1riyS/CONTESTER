d549aef89ae586ecb9c5095ca5a8e4c254635b36
S1riyS <kirill.ankudinov.94@mail.ru>
2022-01-26 20:33:18 +0500

    Added `super().__init__()` to inherited classes

--- app/contester/errors.py
+++ app/contester/errors.py
@@ -7,22 +7,26 @@ class TestingSystemError(Exception):
 # Server response exception
 class ServerResponseError(TestingSystemError):
     def __init__(self):
+        super().__init__()
         self.message = 'Server Response Error'
 
 
 # Execution exception
 class ExecutionError(TestingSystemError):
     def __init__(self):
+        super().__init__()
         self.message = 'Execution Error'
 
 
 # Wrong answer exception
 class WrongAnswerError(TestingSystemError):
     def __init__(self):
+        super().__init__()
         self.message = 'Wrong Answer'
 
 
 # Wrong answer exception
 class ExecutionTimeoutError(TestingSystemError):
     def __init__(self):
+        super().__init__()
         self.message = 'Timeout Error'
