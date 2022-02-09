321ec57a477809c891c942b1510fded4fb3f2fac
Anthony Votinov <anton120120005@gmail.com>
2022-02-09 22:44:00 +0500

    Added table classes

---
+++ app/data/models.py
@@ -0,0 +1,114 @@
+import datetime
+
+import sqlalchemy
+from sqlalchemy import orm
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    role_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                 sqlalchemy.ForeignKey("roles.id"))
+    grade_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("grades.id"))
+    grade_letter = sqlalchemy.Column(sqlalchemy.String)
+
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    registration_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                          default=datetime.datetime.utcnow)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+
+class Grade(db.Model):
+    __tablename__ = "grades"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+
+    number = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+
+class Topic(db.Model):
+    __tablename__ = "topics"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    grade_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                 sqlalchemy.ForeignKey("grades.id"))
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+
+class Task(db.Model):
+    __tablename__ = "tasks"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    topic_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("topics.id"))
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    text = sqlalchemy.Column(sqlalchemy.Text)
+
+
+class Example(db.Model):
+    __tablename__ = "examples"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    task_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("tasks.id"))
+    example_input = sqlalchemy.Column(sqlalchemy.Text)
+    example_output = sqlalchemy.Column(sqlalchemy.Text)
+
+
+class Test(db.Model):
+    __tablename__ = "tests"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    task_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("tasks.id"))
+    test_input = sqlalchemy.Column(sqlalchemy.Text)
+    test_output = sqlalchemy.Column(sqlalchemy.Text)
+    is_hidden = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+
+class Report(db.Model):
+    __tablename__ = "reports"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    task_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("tasks.id"))
+    text = sqlalchemy.Column(sqlalchemy.Text)
+
+
+class Submission(db.Model):
+    __tablename__ = "submissions"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    task_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("tasks.id"))
+    language = sqlalchemy.Column(sqlalchemy.String)
+    passed_tests = sqlalchemy.Column(sqlalchemy.Integer)
+    source_code = sqlalchemy.Column(sqlalchemy.Text)
+    submission_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                          default=datetime.datetime.utcnow)
