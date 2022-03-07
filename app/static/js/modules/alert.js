59484381d7f318374ddcf86cc34fc93576a09358
S1riyS <kirill.ankudinov.94@mail.ru>
2022-03-07 23:34:34 +0500

    FEATURE(alerts): changed style and parameters

--- app/static/js/modules/alert.js
+++ app/static/js/modules/alert.js
@@ -2,7 +2,8 @@ export function showAlert(message, type) {
     $.bootstrapGrowl(message, {
         type: type,
         offset: {from: 'bottom', amount: 20},
-        delay: 3000,
+        delay: 2500,
+        width: 400,
         allow_dismiss: true,
         stackup_spacing: 10
     })
