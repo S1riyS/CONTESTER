6c020df6c70e3ffa6e20737fdc7392baf441b4b2
S1riyS <kirill.ankudinov.94@mail.ru>
2022-03-13 19:57:13 +0500

    FEATURE(auth): added field to select grade

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import PasswordField, StringField, SubmitField, SelectField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length
 
+from app import db
+from app.models import Grade
+
 
 class SignUpForm(FlaskForm):
+    grades = db.session.query(Grade).all()
+    grades_list = [(grade.id, grade.number) for grade in grades]
+
     first_name = StringField('Имя', validators=[DataRequired()])
     last_name = StringField('Фамилия', validators=[DataRequired()])
+    grade = SelectField('Класс', choices=grades_list)
+    letter = SelectField('Буква', choices=['A', 'Б', 'В', 'Г'])
     email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
