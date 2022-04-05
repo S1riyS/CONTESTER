5578a2aff8e84d222dfac4106d8312982f85249a
S1riyS <kirill.ankudinov.94@mail.ru>
2022-04-05 19:30:09 +0500

    FEATURE(auth): added a page with results of email verification

--- app/blueprints/auth/auth.py
+++ app/blueprints/auth/auth.py
@@ -1,30 +1,47 @@
-from flask import Blueprint, render_template, make_response, jsonify
-from itsdangerous import SignatureExpired
+from flask import Blueprint, render_template, redirect, url_for, make_response, jsonify
+from flask_login import current_user
+from itsdangerous import SignatureExpired, BadSignature
 
 from app import db, serializer
 from app.models import User, Role
 from app.forms.auth import LoginForm, SignUpForm
 
-
 auth = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
     return render_template('auth/login.html', title='Вход', form=form)
 
+
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup_page():
     form = SignUpForm()
     return render_template('auth/sign_up.html', title='Регистрация', form=form)
 
+
 @auth.route('/confirm-email/<string:token>')
 def confirm_email(token):
-    try:
-        email = serializer.loads(token, salt='confirm-email', max_age=3600)
+    if not current_user.verified:
+        try:
+            email = serializer.loads(token, salt='confirm-email', max_age=3600)
 
+        except BadSignature:
+            return render_template('auth/after_confirm.html',
+                                   title='Подтверждение почты',
+                                   status='Несуществующий токен подтверждения!',
+                                   success=False)
 
-    except SignatureExpired:
-        return '<h1>The token is expired</h1>'
+        except SignatureExpired:
+            return render_template('auth/after_confirm.html',
+                                   title='Подтверждение почты',
+                                   status='Время подтверждения истекло',
+                                   success=False)
 
-    return '<h1>The token works</h1>'
+        return render_template('auth/after_confirm.html',
+                               title='Подтверждение почты',
+                               status='Ваша почта подтверждена!',
+                               success=True)
+
+    return redirect(url_for('home_page'))
