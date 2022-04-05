60dad2591f5e69f161c053e336d6e4a479cd0e23
S1riyS <kirill.ankudinov.94@mail.ru>
2022-04-05 19:28:42 +0500

    STYLE(utils): PEP8 accordance

--- app/utils/email.py
+++ app/utils/email.py
@@ -1,6 +1,7 @@
 from flask_mail import Message
 from app import app, mail
 
+
 def send_email(subject, recipients, text_body, html_body):
     message = Message(subject, recipients=recipients)
     message.body = text_body
