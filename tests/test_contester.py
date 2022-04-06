db00716a29a7cbac9877b84d9089f298dcca2e84
S1riyS <kirill.ankudinov.94@mail.ru>
2022-04-06 19:11:40 +0500

    FIX(tests): fixed path to `contester` module

--- tests/test_contester.py
+++ tests/test_contester.py
@@ -3,7 +3,7 @@ This is file with tests for Contester class (core of testing system)
 """
 import unittest
 
-from contester.contester import Contester, Status
+from app.contester.contester import Contester, Status
 
 
 class ContesterTests(unittest.TestCase):
