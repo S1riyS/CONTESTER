2ca7a318d0d0d8790799597edd1ec6fa7d426bc7
S1riyS <kirill.ankudinov.94@mail.ru>
2022-04-15 17:36:31 +0500

    CHORE(base): deleted scroll when loading

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,18 +1,5 @@
 import {showAlert} from "./modules/alert.js";
 
-let cords = ['scrollX', 'scrollY'];
-// сохраняем позицию скролла в localStorage
-window.addEventListener('beforeunload', e => cords.forEach(cord => localStorage[cord] = window[cord]));
-// вешаем событие на загрузку (ресурсов) страницы
-window.addEventListener('load', e => {
-    // если в localStorage имеются данные
-    if (localStorage[cords[0]]) {
-        // скроллим к сохраненным координатам
-        window.scroll(...cords.map(cord => localStorage[cord]));
-        // удаляем данные с localStorage
-        cords.forEach(cord => localStorage.removeItem(cord));
-    }
-});
 
 $(function () {
     $('[data-tooltip="tooltip"]').tooltip({
