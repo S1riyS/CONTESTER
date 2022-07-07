c0d282c3ba1fed314b204cdb2f6341060570a6d9
S1riyS <kirill.ankudinov.94@mail.ru>
2022-07-07 14:49:41 +0500

    FEATURE(utils): added global `get_task_url` function

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,4 +1,6 @@
-from app import db
+from flask import url_for
+
+from app import app, db
 from app.models import Grade, Topic, Task
 
 
@@ -8,3 +10,16 @@ def get_task(grade_number, topic_translit_name, task_translit_name):
         Topic.translit_name == topic_translit_name,
         Task.translit_name == task_translit_name
     ).first_or_404()
+
+
+def get_task_url(task: Task):
+    topic = task.topic
+    grade = topic.grade
+    arguments = {
+        'task_translit_name': task.translit_name,
+        'topic_translit_name': topic.translit_name,
+        'grade_number': grade.number
+    }
+    return url_for('problems.task_page', **arguments)
+
+app.jinja_env.globals.update(get_task_url=get_task_url)
