7c1f557123de73a11599808e2906087f2ab6c080
S1riyS <kirill.ankudinov.94@mail.ru>
2022-07-11 13:47:48 +0500

    REFACTOR(contester): exceptions renamed

--- app/contester/exceptions.py
+++ app/contester/exceptions.py
@@ -1,32 +1,32 @@
 # General exception of testing
-class TestingSystemError(Exception):
+class ContesterError(Exception):
     def __init__(self):
         self.message = 'Testing system error'
 
 
 # Server response exception
-class ServerResponseError(TestingSystemError):
+class ApiServiceError(ContesterError):
     def __init__(self):
         super().__init__()
         self.message = 'Server Response Error'
 
 
 # Execution exception
-class ExecutionError(TestingSystemError):
+class ExecutionError(ContesterError):
     def __init__(self):
         super().__init__()
         self.message = 'Execution Error'
 
 
 # Wrong answer exception
-class WrongAnswerError(TestingSystemError):
+class WrongAnswerError(ContesterError):
     def __init__(self):
         super().__init__()
         self.message = 'Wrong Answer'
 
 
 # Wrong answer exception
-class TimeLimitError(TestingSystemError):
+class TimeOutError(ContesterError):
     def __init__(self):
         super().__init__()
         self.message = 'Time Limit Error'
