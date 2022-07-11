02b787d216df445956fec03cd1f01b79f6c71850
S1riyS <kirill.ankudinov.94@mail.ru>
2022-07-11 15:06:29 +0500

    FEATURE(contester): types moved to separated module

---
+++ app/contester/types.py
@@ -0,0 +1,18 @@
+import typing as t
+
+from app.models import Test
+
+from .languages import Language
+
+class SingleTestResult(t.NamedTuple):
+    message: str
+    success: bool
+    test: Test
+    user_output: str
+
+
+class ContesterResponse(t.NamedTuple):
+    language: Language
+    tests: t.Iterable[SingleTestResult]
+    passed_tests: int
+    time: str
