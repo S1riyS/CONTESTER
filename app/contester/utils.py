c1a8972c31d08f687e651e1fbf9c9463e778a35f
S1riyS <kirill.ankudinov.94@mail.ru>
2022-07-12 15:35:10 +0500

    REFACTOR(contester): `silence_event_loop_closed` moved to local utils of contester module

--- app/contester/utils.py
+++ app/contester/utils.py
@@ -1,6 +1,27 @@
 import typing as t
+import platform
+from functools import wraps
+
+from asyncio.proactor_events import _ProactorBasePipeTransport
 
 from .types import SingleTestResult
 
+
 def get_number_of_passed_tests(tests: t.Iterable[SingleTestResult]) -> int:
     return len([result for result in tests if result.success])
+
+
+def silence_event_loop_closed(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except RuntimeError as e:
+            if str(e) != 'Event loop is closed':
+                raise
+
+    return wrapper
+
+
+if platform.system() == 'Windows':
+    _ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
