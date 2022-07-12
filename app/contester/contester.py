a724d4a59e0579e336fbcb2d7f61bd3ad57f3e97
S1riyS <kirill.ankudinov.94@mail.ru>
2022-07-12 15:36:16 +0500

    REFACTOR(contester): work with DB moved to separated module

--- app/contester/contester.py
+++ app/contester/contester.py
@@ -4,75 +4,22 @@ import pprint
 
 import asyncio
 import aiohttp
-from flask_login import current_user
 
-from app import app, db
-from app.models import User, Submission, TestResult, Task, Test
-from app.utils.fix_asyncio import silence_event_loop_closed
+from app import app # TODO: import app.config as app_config
+from app.models import User, Task, Test
 from app.utils.singleton import SingletonBaseClass
 
 from .api_service import ApiCall, ApiCallData
 from .languages import Language, languages
-from .utils import get_number_of_passed_tests
+from .db_manager import save_to_database
+from .utils import get_number_of_passed_tests, silence_event_loop_closed
 from .types import SingleTestResult, ContesterResponse
-from .exceptions import ContesterError, ApiServiceError, ExecutionError, WrongAnswerError, TimeOutError
+from .exceptions import ContesterError
 
 
 class Contester(metaclass=SingletonBaseClass):
-    def __init__(self, TESTING_MODE=False):
+    def __init__(self, TESTING_MODE: bool = False):
         self.TESTING_MODE = TESTING_MODE
-
-    @staticmethod
-    def __save_to_database(
-            task: Task,
-            code: str,
-            response: ContesterResponse,
-            language: str,
-            partner: t.Optional[User] = None
-    ) -> None:
-        """Saves all information about user's submission in database"""
-        submission = Submission(
-            task_id=task.id,
-            language=language,
-            passed_tests=response.passed_tests,
-            source_code=code
-        )
-        db.session.add(submission)
-        db.session.commit()
-
-        current_user.submissions.append(submission)
-        if partner:
-            partner.submissions.append(submission)
-        db.session.commit()
-
-        for result in response.tests:
-            test_result = TestResult(
-                test_id=result.test.id,
-                submission_id=submission.id,
-                success=result.success,
-                message=result.message,
-                user_output=result.user_output
-            )
-            db.session.add(test_result)
-        db.session.commit()
-
-    def load_from_db(self, submission: Submission) -> ContesterResponse:
-        """ Returns `ContesterResponse` object with all data of submission"""
-        results_array = []
-        for result in submission.test_results:
-            results_array.append(SingleTestResult(
-                message=result.message,
-                success=result.success,
-                user_output=result.user_output,
-                test=result.test
-            ))
-
-        return ContesterResponse(
-            language=languages.get_language(submission.language, object_only=True),
-            tests=results_array,
-            passed_tests=get_number_of_passed_tests(results_array),
-            time='None sec'
-        )
 
     @staticmethod
     async def __run_single_test(
@@ -133,6 +80,12 @@ class Contester(metaclass=SingletonBaseClass):
 
                 # Results of testing
                 tests = sorted(test_results, key=lambda item: item.success)
+                print(ContesterResponse(
+                    language=current_language,
+                    tests=tests,
+                    passed_tests=get_number_of_passed_tests(tests),
+                    time="{0:.3f} sec".format(end_time - start_time)
+                ))
                 return ContesterResponse(
                     language=current_language,
                     tests=tests,
@@ -148,21 +101,15 @@ class Contester(metaclass=SingletonBaseClass):
             language: str,
             task: Task,
             partner: t.Optional[User] = None
-    ) -> dict:
-        """
-        :param code: User's code
-        :param language: Programming language
-        :param task: Task object
-        :param partner: User object (person who helped to write solution)
-        :return: Dictionary with the results of testing the program
-        """
+    ) -> ContesterResponse:
+        """Returns results of testing"""
 
         loop = asyncio.new_event_loop()  # Creating async loop
         response = loop.run_until_complete(self.__get_testing_results(code, language, task))
 
         # Saving results to DB
-        # if not self.TESTING_MODE:
-        #     self.__save_to_database(task=task, code=code, response=response, language=language, partner=partner)
+        if not self.TESTING_MODE:
+            save_to_database(task=task, code=code, response=response, language=language, partner=partner)
         # Printing response
         if app.config.get('TESTING'):
             pprint.pprint(response, indent=4)
---
+++ app/contester/db_manager.py
@@ -0,0 +1,62 @@
+import typing as t
+
+from flask_login import current_user
+
+from app import db
+from app.models import User, Task, Submission, TestResult
+from .languages import languages
+from .types import ContesterResponse, SingleTestResult
+from .utils import get_number_of_passed_tests
+
+
+def save_to_database(
+        task: Task,
+        code: str,
+        response: ContesterResponse,
+        language: str,
+        partner: t.Optional[User] = None
+) -> None:
+    """Saves all information about user's submission in database"""
+    submission = Submission(
+        task_id=task.id,
+        language=language,
+        passed_tests=response.passed_tests,
+        source_code=code
+    )
+    db.session.add(submission)
+    db.session.commit()
+
+    current_user.submissions.append(submission)
+    if partner:
+        partner.submissions.append(submission)
+    db.session.commit()
+
+    for result in response.tests:
+        test_result = TestResult(
+            test_id=result.test.id,
+            submission_id=submission.id,
+            success=result.success,
+            message=result.message,
+            user_output=result.user_output
+        )
+        db.session.add(test_result)
+    db.session.commit()
+
+
+def load_from_db(submission: Submission) -> ContesterResponse:
+    """ Returns `ContesterResponse` object with all data of submission"""
+    results_array = []
+    for result in submission.test_results:
+        results_array.append(SingleTestResult(
+            message=result.message,
+            success=result.success,
+            user_output=result.user_output,
+            test=result.test
+        ))
+
+    return ContesterResponse(
+        language=languages.get_language(submission.language, object_only=True),
+        tests=results_array,
+        passed_tests=get_number_of_passed_tests(results_array),
+        time='None sec'
+    )
