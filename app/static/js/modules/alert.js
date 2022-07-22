697cc8e7869edf1d3322c81ec66755b21d4904f8
S1riyS <kirill.ankudinov.94@mail.ru>
2022-07-22 23:30:21 +0500

    REFACTOR(alerts): changed duration of alert

--- app/static/js/modules/alert.js
+++ app/static/js/modules/alert.js
@@ -2,7 +2,7 @@ export function showAlert(message, type) {
     $.bootstrapGrowl(message, {
         type: type,
         offset: {from: 'bottom', amount: 20},
-        delay: 2500,
+        delay: 5000,
         width: 400,
         allow_dismiss: true,
         stackup_spacing: 10
