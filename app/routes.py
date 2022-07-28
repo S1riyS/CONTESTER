018ac89a316bb0fe23937f2261a4a533663927d0
S1riyS <kirill.ankudinov.94@mail.ru>
2022-07-28 17:31:09 +0500

    FEATURE(profile): one function handles `/profile` and `/user/<id>`

--- app/routes.py
+++ app/routes.py
@@ -55,47 +55,51 @@ def submission_page(submission_id):
     abort(404)
 
 
-@app.route('/profile', methods=['GET', 'POST'])
-@register_breadcrumb(app, '.profile', 'Профиль')
-@login_required
-def profile_page():
-    form = EditProfileForm(obj=current_user)
-    init_grades_select(form)
-
-    if form.validate_on_submit():
-        current_user.surname = form.surname.data.capitalize()
-        current_user.name = form.name.data.capitalize()
-        current_user.grade_id = form.grade_id.data
-        current_user.grade_letter = form.grade_letter.data
-        db.session.commit()
-
-        return redirect(url_for('profile_page'))
-
-    context = {
-        'submissions': current_user.submissions,
-        'show_task': True,
-        'user': current_user,
-        'visitor_mode': False,
-        'form': form
-    }
-
-    return render_template('profile.html', title='Профиль', **context)
-
-
-@app.route('/user/<int:user_id>', methods=['GET'])
+@app.route('/profile', defaults={'user_id': None}, methods=['GET', 'POST'])
+@app.route('/user/<int:user_id>', methods=['GET', 'POST'])
 @register_breadcrumb(app, '.user', '', dynamic_list_constructor=bc.view_user_dlc)
 @login_required
-def user_page(user_id):
-    user = db.session.query(User).get_or_404(user_id)
+def profile_page(user_id):
+    # User identification
+    if user_id is not None:
+        user: User = db.session.query(User).get_or_404(user_id)
+        edit_permission = False
 
-    if user.id == current_user.id:
-        return redirect(url_for('profile_page'))
+        # Redirecting to /profile if user_id from /user/<user_id> is current user's id
+        if user.id == current_user.id:
+            return redirect(url_for('profile_page', user_id=None))
+    else:
+        user: User = current_user
+        edit_permission = True
 
-    context = {
+    # Giving permission to edit user's profile if current user is admin
+    if current_user.is_admin:
+        edit_permission = True
+
+    # Form initialization
+    form = EditProfileForm(obj=user)
+    init_grades_select(form)
+
+    # Handling form submission
+    if form.validate_on_submit():
+        user.surname = form.surname.data.capitalize()
+        user.name = form.name.data.capitalize()
+        user.grade_id = form.grade_id.data
+        user.grade_letter = form.grade_letter.data
+        db.session.commit()
+
+        return redirect(url_for('profile_page', user_id=user.id))
+
+    # Forming table data
+    table_data = {
         'submissions': user.submissions,
-        'show_task': True,
+        'show_task': True
+    }
+    # Forming page context
+    context = {
         'user': user,
-        'visitor_mode': True
+        'form': form,
+        'edit_permission': edit_permission,
     }
 
-    return render_template('profile.html', title='Ученик', **context)
+    return render_template('profile.html', title=f'{user.surname} {user.name}', **table_data, **context)
