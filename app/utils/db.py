fba81abeed25187c2a7eb7d542fac730cfa67d3d
S1riyS <kirill.ankudinov.94@mail.ru>
2022-08-04 15:51:53 +0500

    REFACTOR: `get_task_url` function replaced with Task model property

--- app/utils/db.py
+++ app/utils/db.py
@@ -14,17 +14,3 @@ def get_task(grade_number, topic_translit_name, task_translit_name):
         Task.translit_name == task_translit_name
     ).first_or_404()
 
-
-def get_task_url(task: Task, tab: t.Optional[str] = None):
-    topic = task.topic
-    grade = topic.grade
-    arguments = {
-        'task_translit_name': task.translit_name,
-        'topic_translit_name': topic.translit_name,
-        'grade_number': grade.number,
-        'tab': tab
-    }
-    return url_for('problems.task_page', **arguments)
-
-
-app.jinja_env.globals.update(get_task_url=get_task_url)
