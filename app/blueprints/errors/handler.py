19d135e782f33cee8a271d0db9ba7cf32dbdf7ef
S1riyS <kirill.ankudinov.94@mail.ru>
2022-08-04 14:01:01 +0500

    FIX(errors): added a missing `url_prefix' when initializing the blueprint

--- app/blueprints/errors/handler.py
+++ app/blueprints/errors/handler.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-errors = Blueprint('errors', __name__, template_folder='templates', static_folder='static')
+errors = Blueprint('errors', __name__, template_folder='templates', static_folder='static', url_prefix='/errors')
 
 errors_data = {
     403: {
