6e4f5090ece998831d66d3fedb912be1425adcf3
S1riyS <kirill.ankudinov.94@mail.ru>
2022-08-04 15:53:52 +0500

    REFACTOR(utils): deleted useless imports, added type hinting

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,16 +1,12 @@
-import typing as t
-
-from flask import current_app as app
 from flask import url_for
 
 from app import db
 from app.models import Grade, Topic, Task
 
 
-def get_task(grade_number, topic_translit_name, task_translit_name):
+def get_task(grade_number: str, topic_translit_name: str, task_translit_name: str) -> Task:
     return db.session.query(Task).filter(
         Grade.number == grade_number,
         Topic.translit_name == topic_translit_name,
         Task.translit_name == task_translit_name
     ).first_or_404()
-
