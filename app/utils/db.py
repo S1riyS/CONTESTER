14a696a4a1889bbfa2d2c0c7cb7c7ebbdd67ca44
S1riyS <kirill.ankudinov.94@mail.ru>
2022-08-04 15:54:24 +0500

    FIX(utils): fixed type of `grade_number` in `get_task` function

--- app/utils/db.py
+++ app/utils/db.py
@@ -4,7 +4,7 @@ from app import db
 from app.models import Grade, Topic, Task
 
 
-def get_task(grade_number: str, topic_translit_name: str, task_translit_name: str) -> Task:
+def get_task(grade_number: int, topic_translit_name: str, task_translit_name: str) -> Task:
     return db.session.query(Task).filter(
         Grade.number == grade_number,
         Topic.translit_name == topic_translit_name,
